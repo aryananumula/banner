f4dd97d9dfe77c67757afaffd1c269a08f828470
Aryan A <aryananumula@gmail.com>
2025-02-28 17:55:35 -0800

    change subsytem

--- src/main/java/frc/robot/subsystems/BannerSubstem.java
+++ src/main/java/frc/robot/subsystems/BannerSubstem.java
@@ -6,13 +6,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.bannerSensor;
 
 public class BannerSubstem extends SubsystemBase {
-  public bannerSensor banner;
+
   /** Creates a new ExampleSubsystem. */
   public BannerSubstem() {
-    banner = new bannerSensor(this);
   }
 
   /**
@@ -30,7 +28,8 @@ public class BannerSubstem extends SubsystemBase {
   }
 
   /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   * An example method querying a boolean state of the subsystem (for example, a
+   * digital sensor).
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
@@ -41,7 +40,6 @@ public class BannerSubstem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    banner.schedule();
   }
 
   @Override
